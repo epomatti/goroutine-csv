f425b0cff6336270f09cac919c68cd2b423c7b7a
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-05-10 01:05:56 -0300

    process

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Requirements:
 - Each CSV line should be processed concurrently
 - If a single CSV line is invalid the function should be canceled
 - Goroutines should stop if a CSV line is invalid
+
+https://gobyexample.com/goroutines
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sync"
+)
 
+func main() {
+	file := open()
+	defer file.Close()
+
+	process(file)
+}
+
+func open() *os.File {
+	file, err := os.Open("data/valid.csv")
+	if err != nil {
+		panic(err)
+	}
+	return file
+}
+
+func process(file *os.File) {
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var wg sync.WaitGroup
+	for scanner.Scan() {
+		data := scanner.Text()
+		wg.Add(1)
+		go func(data string) {
+			fmt.Println(data)
+			wg.Done()
+		}(data)
+	}
+	wg.Wait()
 }
